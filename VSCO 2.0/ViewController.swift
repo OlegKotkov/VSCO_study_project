01ee60647a1c734fc98e0e6d601fd1e70658e4aa
OlegKotkov <tigrastov@gmail.com>
2022-03-01 13:07:30 +0300

    Initial Commit

---
+++ VSCO 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VSCO 2.0
+//
+//  Created by mac on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
